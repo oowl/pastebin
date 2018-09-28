eba46b3f7e7a9b3dc1ef8e3289e7b643e58b48e0
qwertyuiop6 <super771783057@foxmail.com>
2018-09-28 23:28:53 +0800

    change theme

---
+++ static/js/richman.js
@@ -0,0 +1,28 @@
+var a_idx = 0;
+jQuery(document).ready(function($) {
+  $("body").click(function(e) {
+    var a=['#1793d3','#f57421','#c70036','purple','#10baee','#294172','#87cf3e','#73ba25','#fb3920','#8d86bb','#34be5b'];
+    var $i = $("<span/>").text("+1s");
+    a_idx = (a_idx + 1) % a.length;
+    var x = e.pageX,
+    y = e.pageY;
+    $i.css({
+      "z-index": 999,
+      "top": y - 20,
+      "left": x,
+      "position": "absolute",
+      "font-weight": "bold",
+      "color": a[a_idx]
+    });
+    $("body").append($i);
+    $i.animate({
+      "top": y - 180,
+      "opacity": 0
+    },
+    1500,
+    function() {
+      $i.remove();
+    });
+    a_idx = (a_idx + 1) % a.length;
+  });
+});
