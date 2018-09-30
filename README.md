fa3548fefa3b7061916e38906572b7dfd812a84d
qwertyuiop6 <super771783057@foxmail.com>
2018-09-30 16:14:57 +0800

    delete some useless css and change readme

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # pastebin
-一个来源于rocket demon的pastebin
+一个源于 =>[rocket](https://rocket.rs/) demon的pastebin
+代码高亮 => [highlightjs](https://github.com/highlightjs/highlight.js)
--- static/js/richman.js
+++ static/js/richman.js
@@ -50,7 +50,7 @@ jQuery(document).ready(function ($) {
         if (rest[0]) {
             $info.text(rest[1]);
             $info.css('color', "#55a96f");
-                             let X=$('.flyowl').offset().left;
+            let X=$('.flyowl').offset().left;
             $(".flyowl").animate({'left':screen.width-X},'slow',function(e){
                 $(".flyowl").css('left',-X).animate({'left':0},'fast')
             })
